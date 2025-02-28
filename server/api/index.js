c283a169051a1caee3903ba1d4bac306c64f93a5
mstevens843 <mathewstevens7457@gmail.com>
2025-02-28 13:54:41 -0800

    i

--- server/api/index.js
+++ server/api/index.js
@@ -2,19 +2,15 @@ const express = require('express');
 
 // Import individual route modules
 const authRoutes = require('./auth');
-const walletRoutes = require('./wallet');
 const postRoutes = require('./posts');
 const commentRoutes = require('./comments');
-const tipRoutes = require('./tips');
 const userRoutes = require('./users');
-const transactionRoutes = require('./transactions');
 const tradeRoutes = require('./trade'); // Correct import of trade.js
 const messageRoutes = require('./messages'); // Added messages route
 const notificationRoutes = require('./notifications'); // Added notifications route
 const trendingCryptoRoutes = require('./trendingCrypto'); // Added trendingCrypto route
 const searchRoutes = require('./search'); // Added search route
 const testRoutes = require('./test'); // Import the test route
-const testWebSocketRouter = require("./test-websocket");
 const jupiterRoutes = require('./jupiter'); 
 
 
@@ -29,18 +25,12 @@ const router = express.Router();
 console.log("Registering routes...");
 
 router.use('/auth', authRoutes); // Authentication and authorization
-router.use('/wallet', walletRoutes); // Wallet operations (e.g., balance, transactions)
 router.use('/posts', postRoutes); // Posts (e.g., create, update, fetch)
 router.use('/comments', commentRoutes.router); // Comments and WebSocket logic
 console.log(commentRoutes);
-router.use('/tips', tipRoutes); // Tipping functionality
 router.use('/users', userRoutes); // User profile and account management
-router.use('/transactions', transactionRoutes); // Transactions and history
 router.use('/trade', tradeRoutes); // Trading-related routes
-router.use("/socket", testWebSocketRouter);
 router.use('/jupiter', jupiterRoutes); 
-
-
 
 // Log when the /trade route is registered
 console.log('/trade route successfully registered');
